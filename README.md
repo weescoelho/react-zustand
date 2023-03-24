e8d8650264bd7a0e6d7ae89ad100181ab3437597
Weslley Coelho <wes.coelho18@gmail.com>
2023-03-24 20:01:56 -0300

    Implementação de exemplo para persistencia de dados.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Repo de exemplo da utilização do Zustand como state manager
 
 [zustand] - https://github.com/pmndrs/zustand
+[zustand-docs] - https://docs.pmnd.rs/zustand/getting-started/introduction
--- src/stores/useTodoStore.ts
+++ src/stores/useTodoStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export type Todo = {
   id: string;
@@ -12,13 +13,23 @@ type TodoStore = {
   toggleTodo: (id: string) => void;
 };
 
-export const useTodoStore = create<TodoStore>((set) => ({
-  todos: [],
-  addTodo: (todo: Todo) => set((state) => ({ todos: [...state.todos, todo] })),
-  toggleTodo: (id: string) =>
-    set((state) => {
-      const todoIndex = state.todos.findIndex((todo) => todo.id === id);
-      state.todos[todoIndex].done = !state.todos[todoIndex].done;
-      return { todos: state.todos };
+// O uso do método persist, faz com que o estado seja persistido no navegador do usuário
+
+export const useTodoStore = create(
+  persist<TodoStore>(
+    (set) => ({
+      todos: [],
+      addTodo: (todo: Todo) =>
+        set((state) => ({ todos: [...state.todos, todo] })),
+      toggleTodo: (id: string) =>
+        set((state) => {
+          const todoIndex = state.todos.findIndex((todo) => todo.id === id);
+          state.todos[todoIndex].done = !state.todos[todoIndex].done;
+          return { todos: state.todos };
+        }),
     }),
-}));
+    {
+      name: "todo-storage",
+    },
+  ),
+);
